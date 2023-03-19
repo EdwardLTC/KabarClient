e2ffbfbb3eb254ace57840a9b8132654d68eac7b
EdwardLTC <88788257+EdwardLTC@users.noreply.github.com>
2023-03-19 14:57:39 +0700

    fixx

--- android/app/build/generated/source/buildConfig/debug/com/fservice/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/fservice/BuildConfig.java
@@ -5,7 +5,7 @@ package com.fservice;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.fservice";
+  public static final String APPLICATION_ID = "com.edward";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
@@ -14,15 +14,15 @@ public final class BuildConfig {
   // Field from default config.
   public static final String APP_BUILD_CODE_IOS = "1";
   // Field from default config.
-  public static final String APP_ID_ANDROID = "com.fservice";
+  public static final String APP_ID_ANDROID = "com.edward";
   // Field from default config.
-  public static final String APP_ID_IOS = "com.fpoly.service";
+  public static final String APP_ID_IOS = "com.ltc.edward";
   // Field from default config.
-  public static final String APP_NAME = "FService";
+  public static final String APP_NAME = "Edward";
   // Field from default config.
   public static final String APP_VERSION = "1.0.0";
   // Field from default config.
-  public static final String BASE_URL = "https://urlserver.com";
+  public static final String BASE_URL = "https://fpoly-hcm.herokuapp.com/api/";
   // Field from default config.
   public static final String ENV = "PRODCUTION";
   // Field from default config.
